6d5f536f3cc6ad8e9fd15ddd6d2b0fe55115c2fe
jams <jams.smith@cambridge.ai>
2024-06-25 21:06:46 +0800

    card charging

--- example/pages/CardPage.vue
+++ example/pages/CardPage.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="page w-full h-full d-center black-page">
-    <mu-flip-card></mu-flip-card>
+    <div class="d-flex">
+      <mu-flip-card v-for="k in 4" class="ml-1" :key="k"> 
+        <template #flip-up> {{k+1}} </template>
+        <template #flip-down> {{k+1}} </template>
+        <template #up> {{k+2}} </template>
+        <template #down> {{k+2}} </template>
+      </mu-flip-card>
+    </div>
   </div>
 </template>
-<script lang="ts" setup>
-</script>
+<script lang="ts" setup></script>
 <style lang="scss">
 html,
 body {
--- src/components/card/FlipCard.vue
+++ src/components/card/FlipCard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="mu-flip-card">
-    <div class="flip-card flip-card1">2</div>
-    <div class="flip-card flip-card2">2</div>
-    <div class="flip-card flip-card3">3</div>
-    <div class="flip-card flip-card4">3</div>
+    <div class="flip-card flip-card1"><slot name="flip-up"/></div>
+    <div class="flip-card flip-card2"><slot name="flip-down"/></div>
+    <div class="flip-card flip-card3"><slot name="up"/></div>
+    <div class="flip-card flip-card4"><slot name="down"/></div>
   </div>
 </template>
 <script lang="ts" setup>
